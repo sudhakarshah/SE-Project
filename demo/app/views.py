97be0ffe0c4cf01cccedbb51663a0e1cbef5afb9
louiswnlo <louiswn.lo@tinklabs.com>
2018-10-28 17:28:13 +0800

    add demo photo and create a order cart

--- demo/app/views.py
+++ demo/app/views.py
@@ -7,4 +7,13 @@ def index(request):
     return render(request, 'signin.html')
 
 def browse(request):
-    return render(request, 'browse.html')
+	# query from db
+	# get all data of medicines
+	# if request.POST
+	items= Item.objects.all()
+	Categories = Category.objects.all()
+	context = {
+        'item_list': items,
+		'category_list': Categories,
+    }
+	return render(request, 'browse.html',context)
