f2173389e100e09d9d14fd14fafc65566e6c874e
sudhakarshah <sudhakarshah2013@gmail.com>
2018-10-28 17:11:28 +0800

    test

--- demo/app/models.py
+++ demo/app/models.py
@@ -6,6 +6,7 @@ def get_image_path(instance, filename):
     return os.path.join('photos', str(instance.id), filename)
 
 
+
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
