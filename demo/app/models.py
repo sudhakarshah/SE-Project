cf40b8e157a5fd07a35eddb50ac86b0c9b4d8371
sudhakarshah <sudhakarshah2013@gmail.com>
2018-10-28 17:24:16 +0800

    migration of order table

--- demo/app/models.py
+++ demo/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import os
+from datetime import datetime
 # Create your models here.
 
 def get_image_path(instance, filename):
@@ -16,20 +17,6 @@ class Item(models.Model):
     image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
     weight = models.DecimalField(max_digits=5, decimal_places=2)
 
-class User(models.Model):
-    name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200, blank=True)
-    email = models.EmailField(max_length=254, unique=True)
-    password = models.CharField(max_length=200)
-    ROLE_CHOICES = (
-        ('CLINIC_MANAGER', 'Clinic Manager'),
-        ('WAREHOUSE_PERSONNEL', 'Warehouse Personnel'),
-        ('DISPATCHER', 'Dispatcher'),
-    )
-    role = models.CharField(max_length=200,choices=ROLE_CHOICES,default='CLINIC_MANAGER')
-
-    def __str__(self):
-        return self.name
 
 class HospitalLocation(models.Model):
     name = models.CharField(max_length=200)
@@ -45,13 +32,27 @@ class ClinicLocation(models.Model):
     supplying_hospital = models.ForeignKey(HospitalLocation, on_delete=models.CASCADE, null=True)
     distance_from_supplying_hospital = models.DecimalField(max_digits=4, decimal_places=2)
 
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=200)
+    ROLE_CHOICES = (
+        ('CLINIC_MANAGER', 'Clinic Manager'),
+        ('WAREHOUSE_PERSONNEL', 'Warehouse Personnel'),
+        ('DISPATCHER', 'Dispatcher'),
+    )
+    role = models.CharField(max_length=200,choices=ROLE_CHOICES,default='CLINIC_MANAGER')
+    clinic_location = models.ForeignKey(ClinicLocation, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
 class InterClinicDistance(models.Model):
     location_a = models.ForeignKey(ClinicLocation, on_delete=models.CASCADE, null=True, related_name='location_a')
     location_b = models.ForeignKey(ClinicLocation, on_delete=models.CASCADE, null=True, related_name='location_b')
     distance= models.DecimalField(max_digits=4, decimal_places=2)
 
-    def get_distance(a,b):
-        return 0
 
 class Order(models.Model):
     total_weight = models.DecimalField(max_digits=5, decimal_places=2)
@@ -70,4 +71,4 @@ class Order(models.Model):
 class OrderedItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
     item = models.ForeignKey(Item, on_delete=models.CASCADE, null=True)
-    quantity = models.IntegerField(max_digits=4, default=0)
+    quantity = models.IntegerField(default=0)
