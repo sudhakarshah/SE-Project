a27fc513f83422cb9c026ec4f251fe12d7204091
louiswnlo <louiswn.lo@tinklabs.com>
2018-10-28 17:12:47 +0800

    alter git ignore & db & model

--- demo/app/admin.py
+++ demo/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+# For adding dummy record
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(User)
+admin.site.register(HospitalLocation)
+admin.site.register(ClinicLocation)
+admin.site.register(InterClinicDistance)
