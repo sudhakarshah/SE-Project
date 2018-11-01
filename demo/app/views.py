d6e81467a86d1237498c77fd572ea54c3ece3cb7
sudhakarshah <sudhakarshah2013@gmail.com>
2018-11-01 18:02:47 +0800

    path changes done and to_be_loaded init

--- demo/app/views.py
+++ demo/app/views.py
@@ -6,7 +6,7 @@ from .models import *
 
 
 def index(request):
-    return render(request, 'signin.html')
+    return render(request, 'signin/index.html')
 
 def browse_items(request):
 	# query from db
@@ -41,4 +41,7 @@ def browse_items(request):
 			'item_list': items,
 			'category_list': Categories,
 		}
-		return render(request, '/browse_items/index.html',context)
+		return render(request, 'browse_items/index.html', context)
+
+def browse_to_be_loaded(request):
+    return render(request, 'browse_to_be_loaded/index.html')
