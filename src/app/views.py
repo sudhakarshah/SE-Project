912a15b828e47bd5e4ecbba4552866362703c207
louiswnlo <louiswn.lo@tinklabs.com>
2018-11-13 13:03:22 +0800

    implement the logic for post request

--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,8 @@
 import json
+import io
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from reportlab.pdfgen import canvas
 from .models import *
 
 
@@ -49,17 +51,55 @@ def browse_items(request):
         return render(request, 'browse_items/index.html', context)
 
 def browse_to_be_loaded(request):
+	# updating order status to dispatched
+	if request.method == 'POST':
+		orderId = request.POST['orderId']
+		Order.loaded_into_drone(orderId)
+		return HttpResponse('test')
 
-    # updating order status to dispatched
-    if request.method == 'POST':
-        orderId = request.POST['orderId']
-        Order.loaded_into_drone(orderId)
-        return HttpResponse('test')
+	else:
+		# rendering all orders with status QUEUED_FOR_DISPATCH
+		orders = Order.objects.filter(status=Order.STATUS_CHOICES[2][0])
+		context = {
+			'order_list': orders,
+		}
+		return render(request, 'browse_to_be_loaded/index.html', context)
 
-    else:
-        # rendering all orders with status QUEUED_FOR_DISPATCH
-        orders = Order.objects.filter(status=Order.STATUS_CHOICES[2][0])
-        context = {
-            'order_list': orders,
-        }
-        return render(request, 'browse_to_be_loaded/index.html', context)
+def browse_to_be_processed(request):
+
+	if request.method == 'POST':
+
+		event = request.POST['event']
+		orderId = request.POST['orderId']
+
+		if event == 'READY_TO_PROCESS':
+			Order.ready_to_process(orderId)
+		elif event == 'COMPLETE_PROCESSING': 
+			Order.complete_processing(orderId)
+		elif event == 'DOWNLOAD_SHIPPING_LABEL':
+			# Create the HttpResponse object with the appropriate PDF headers.
+			response = HttpResponse(content_type='application/pdf')
+			response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+			# Create the PDF object, using the response object as its "file."
+			p = canvas.Canvas(response)
+
+			# Draw things on the PDF. Here's where the PDF generation happens.
+			# See the ReportLab documentation for the full list of functionality.
+			p.drawString(100, 100, "Hello world.")
+
+			# Close the PDF object cleanly, and we're done.
+			p.showPage()
+			p.save()
+			return response
+
+		return HttpResponse('test')
+
+	else:
+		processOrders = Order.objects.filter(status=Order.STATUS_CHOICES[0][0])
+		packOrders = Order.objects.filter(status=Order.STATUS_CHOICES[1][0])
+		context = {
+			'process_order_list': processOrders,
+			'pack_order_list': packOrders,
+		}
+		return render(request, 'browse_orders_warehouse/index.html', context)
