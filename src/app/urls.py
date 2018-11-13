4cf0d5e14ee4ce9f5a20e20643817ef3ae930ae8
louiswnlo <louiswn.lo@tinklabs.com>
2018-11-13 13:00:55 +0800

    add url for warehouse

--- src/app/urls.py
+++ src/app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('items', views.browse_items),
     path('OrdersToBeLoaded/', views.browse_to_be_loaded),
+	path('OrdersToBeProcessed/', views.browse_to_be_processed),
     path('', views.index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT+'/photos')
